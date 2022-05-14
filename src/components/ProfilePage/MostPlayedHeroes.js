21acc05e15563779c219fbcacaff450148a95dbc
Tom Fielder <tom.fielder97@gmail.com>
2022-05-14 10:34:49 +0100

    Fix most played heroes map, begin building table

--- src/components/ProfilePage/MostPlayedHeroes.js
+++ src/components/ProfilePage/MostPlayedHeroes.js
@@ -6,26 +6,31 @@ import '../../styles/MostPlayedHeroes.css'
 function MostPlayedHeroes() {
   const [Heroes, setHeroes] = useState([])
   const [MostPlayed, setMostPlayed] = useState([])
-  // const [Table, setTable] = useState(['bane'])
   const { ID } = useParams()
 
   useEffect(() => {
     opendota
       .getHeroes()
       .then((result) => {
-        setHeroes(result)
+        setHeroes([...result])
       })
       .then(() => {
         opendota.getMostPlayedHeroes(ID).then((result) => {
-          setMostPlayed(result)
+          setMostPlayed([...result])
         })
       })
   }, [ID])
 
-  const getHeroByID = (heroID) => {
-    console.log(heroID)
-    console.log(Heroes.find((hero) => hero.id === 1))
-    console.log(Heroes.find((hero) => hero.id === `${heroID}`))
+  const getHeroByID = (heroID) =>
+    Heroes.find((hero) => hero.id === parseInt(heroID, 10))
+
+  const timeSinceLastMatch = (date) => {
+    const lastMatchMs = date * 1000
+    const lastMatch = new Date(lastMatchMs)
+    const today = new Date()
+    const dayDiff = Math.round((today - lastMatch) / (1000 * 3600 * 24))
+    const hourDiff = Math.round((today - lastMatch) / (1000 * 3600))
+    return dayDiff >= 1 ? `${dayDiff} days ago` : `${hourDiff} hours ago`
   }
 
   return (
@@ -35,13 +40,30 @@ function MostPlayedHeroes() {
           Most Played Heroes <small>All Time</small>
         </header>
         <article>
-          <div className="r-table r-only-mobile-5 heroes-overview">
+          <div className="r-table heroes-overview">
             {MostPlayed.length && Heroes.length
-              ? MostPlayed.map((hero) => (
+              ? MostPlayed.slice(0, 10).map((hero) => (
                   <div className="r-row" key={hero.hero_id}>
                     <div className="r-body">
                       <div className="image-container image-container-hero image-container-icon">
-                        {getHeroByID(hero.hero_id)}
+                        <img
+                          src={`http://cdn.dota2.com/apps/dota2/images/heroes/${
+                            getHeroByID(hero.hero_id).name.split(
+                              'npc_dota_hero_',
+                            )[1]
+                          }_sb.png`}
+                          alt={getHeroByID(hero.hero_id).localized_name}
+                        ></img>
+                        <div className="subtext minor">
+                          <p>{getHeroByID(hero.hero_id).localized_name}</p>
+                          <p>{timeSinceLastMatch(hero.last_played)}</p>
+                        </div>
+                      </div>
+                      <div className="gamesPlayedAsHero">{hero.games}</div>
+                      <div className="winrateAsHero">
+                        {`${
+                          Math.round((hero.win / hero.games) * 100 * 100) / 100
+                        }%`}
                       </div>
                     </div>
                   </div>
