88080cc4b848c077d0eed667fbade2176f0fede8
Tom Fielder <tom.fielder97@gmail.com>
2022-05-16 00:16:11 +0100

    Basic style for search page. Plan table for search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import * as opendota from '../helpers/opendota'
+import '../styles/search.css'
 
 function Search() {
   const { Name } = useParams()
@@ -10,31 +11,45 @@ function Search() {
     opendota.getPlayersByName(Name).then((result) => SetSearchResult(result))
   }, [Name])
 
+  /* To get the date of their last match, do this
+    {
+      matchTimeUnix = Date.parse(player.last_match_time)
+      lastMatchDate = new Date(matchTimeUnix)
+      return `${lastMatchDate.getDate()}/${lastMatchDate.getMonth() + 1}/${lastMatchDate.getFullYear()}}`
+    }
+  */
+
   return (
     <div id="playerSearchResults">
-      {SearchResult.length !== 0
-        ? SearchResult.map((player) => (
-            <div className="player-result" key={player.account_id}>
-              <div
-                className="inner"
-                data-account-id={player.account_id}
-                data-search-link={`/players/${player.account_id}`}
-              >
-                <div className="avatar">
-                  <div className="image-container image-container-player">
-                    <Link to={`/players/${player.account_id}`}>
-                      <img
-                        className="image-player image-player-avatar"
-                        alt={`${player.personaname}`}
-                        src={player.avatarfull}
-                      ></img>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))
-        : 'Not loaded yet'}
+      <section>
+        <header>{`Search results for: ${Name}`}</header>
+      </section>
+      <article>
+        <table className="r-table search-result">
+          <tr>
+            <th>Player</th>
+            <th>Name</th>
+          </tr>
+          {SearchResult.length !== 0
+            ? SearchResult.map((player) => (
+                <tr className="player-result" key={player.account_id}>
+                  <td className="avatar">
+                    <div className="image-container image-container-player">
+                      <Link to={`/players/${player.account_id}`}>
+                        <img
+                          className="image-player image-player-avatar"
+                          alt={`${player.personaname}`}
+                          src={player.avatarfull}
+                        ></img>
+                      </Link>
+                    </div>
+                  </td>
+                  <td className="green">{player.personaname}</td>
+                </tr>
+              ))
+            : 'Not loaded yet'}
+        </table>
+      </article>
     </div>
   )
 }
